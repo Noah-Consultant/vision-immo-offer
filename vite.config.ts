406b26cb88d907e18bfadbea32719be8773ae4ac
StanislasBASQUIN <stanislas.basquin@gmail.com>
2026-07-10 13:07:57 +0200

    Creation of Action Workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/vision-immo-offer/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
